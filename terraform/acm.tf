836ac46a0208eb1b2b64864b147d34cd701ea92d
koki-develop <kou.pg.0131@gmail.com>
2021-10-10 13:05:54 +0900

    terraform ディレクトリを削除

--- terraform/acm.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_acm_certificate" "main" {
-  domain_name       = local.domain
-  validation_method = "DNS"
-  tags              = { Name = local.prefix }
-}
--- terraform/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "vercel_domain_ip" {}
-
-locals {
-  prefix = "kou-portfolio"
-  domain = "kou-pg.com"
-}
