8660fa4aedd35374bc19d2b70f599603781fc8b2
koki-develop <kou.pg.0131@gmail.com>
2021-10-23 20:38:28 +0900

    404 を pageComponents 側に移動

---
+++ src/pageComponents/NotFound/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { NextPage } from 'next';
+import {
+  Box,
+  Typography,
+} from '@material-ui/core';
+import {
+  createStyles,
+  makeStyles,
+} from '@material-ui/core/styles';
+import Layout from '@/layout';
+
+const useStyles = makeStyles(theme =>
+  createStyles({
+    messageContainer: {
+      marginTop: theme.spacing(2),
+      marginBottom: theme.spacing(2),
+    },
+    message: {
+      fontSize: theme.typography.h5.fontSize,
+      fontWeight: 'bold',
+      textAlign: 'center',
+    },
+  }),
+);
+
+const NotFound: NextPage = () => {
+  const classes = useStyles();
+
+  const message = 'お探しのページは見つかりませんでした';
+
+  return (
+    <Layout
+      title={message}
+      hideMenu
+    >
+      <Box className={classes.messageContainer}>
+        <Typography
+          classes={{
+            root: classes.message,
+          }}
+        >
+          {message}
+        </Typography>
+      </Box>
+    </Layout>
+  );
+};
+
+export default NotFound;
