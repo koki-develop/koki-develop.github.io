7eb5d39d55aadef2c69c49ecd2672c1d26faabfc
koki-develop <kou.pg.0131@gmail.com>
2021-10-24 20:01:38 +0900

    Works を最低限リファクタ

--- src/pageComponents/Home/Works/index.tsx
+++ src/pageComponents/Home/Works/index.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import {
-  Card,
-  CardActionArea,
-  CardContent,
-  CardHeader,
-  CardMedia,
-  Grid,
-  Typography,
-} from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 import {
   createStyles,
   makeStyles,
