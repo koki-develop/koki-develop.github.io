0ea7814ccf94d9b97fa52d36a0676aad74f66919
koki-develop <kou.pg.0131@gmail.com>
2021-12-16 09:18:05 +0900

    ProfileBlock コンポーネントを作成

---
+++ src/components/model/profile/ProfileBlock.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+import { Config } from '@/types/config';
+import Section from '@/components/utils/Section';
+import ExternalLink from '@/components/utils/ExternalLink';
+
+const useStyles = makeStyles(theme =>
+  createStyles({
+    profile: {
+      alignItems: 'center',
+      display: 'flex',
+      flexDirection: 'column',
+      marginBottom: theme.spacing(1),
+    },
+    avatar: {
+      height: 150,
+      width: 150,
+    },
+    name: {
+      fontSize: theme.typography.h5.fontSize,
+    },
+    tag: {
+      color: theme.palette.text.secondary,
+      fontSize: theme.typography.h6.fontSize,
+    },
+    descriptionContainer: {
+      display: 'flex',
+      justifyContent: 'center',
+    },
+    description: {
+      textAlign: 'center',
+      whiteSpace: 'pre',
+    },
+  }),
+);
+
+export type ProfileBlockProps = {
+  config: Config;
+};
+
+const ProfileBlock: React.VFC<ProfileBlockProps> = React.memo(props => {
+  const classes = useStyles();
+
+  const { config } = props;
+
+  return (
+    <div>
+      <div className={classes.profile}>
+        <div>
+          <img
+            className={classes.avatar}
+            src='/images/profile.png'
+            alt={config.profile.name}
+          />
+        </div>
+        <Typography className={classes.name}>{config.profile.name}</Typography>
+        <Typography className={classes.tag}>Developer</Typography>
+      </div>
+
+      <div className={classes.descriptionContainer}>
+        <Typography className={classes.description}>
+          {config.profile.description}
+        </Typography>
+      </div>
+    </div>
+  );
+});
+
+ProfileBlock.displayName = 'ProfileBlock';
+
+export default ProfileBlock;
--- src/components/model/profile/ProfileSection.tsx
+++ src/components/model/profile/ProfileSection.tsx
@@ -1,29 +1,12 @@
 import React from 'react';
-import Typography from '@material-ui/core/Typography';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
 import { Config } from '@/types/config';
+import ProfileBlock from '@/components/model/profile/ProfileBlock';
 import Section from '@/components/utils/Section';
 import ExternalLink from '@/components/utils/ExternalLink';
 
 const useStyles = makeStyles(theme =>
   createStyles({
-    profile: {
-      alignItems: 'center',
-      display: 'flex',
-      flexDirection: 'column',
-      marginBottom: theme.spacing(1),
-    },
-    avatar: {
-      height: 150,
-      width: 150,
-    },
-    name: {
-      fontSize: theme.typography.h5.fontSize,
-    },
-    tag: {
-      color: theme.palette.text.secondary,
-      fontSize: theme.typography.h6.fontSize,
-    },
     socialList: {
       display: 'flex',
       justifyContent: 'center',
@@ -34,14 +17,6 @@ const useStyles = makeStyles(theme =>
     socialIcon: {
       height: 40,
       width: 40,
-    },
-    descriptionContainer: {
-      display: 'flex',
-      justifyContent: 'center',
-    },
-    description: {
-      textAlign: 'center',
-      whiteSpace: 'pre',
     },
   }),
 );
@@ -57,23 +32,7 @@ const AboutSection: React.VFC<AboutSectionProps> = React.memo(props => {
 
   return (
     <Section title='About' hideTitle>
-      <div className={classes.profile}>
-        <div>
-          <img
-            className={classes.avatar}
-            src='/images/profile.png'
-            alt={config.profile.name}
-          />
-        </div>
-        <Typography className={classes.name}>{config.profile.name}</Typography>
-        <Typography className={classes.tag}>Developer</Typography>
-      </div>
-
-      <div className={classes.descriptionContainer}>
-        <Typography className={classes.description}>
-          {config.profile.description}
-        </Typography>
-      </div>
+      <ProfileBlock config={config} />
 
       <ul className={classes.socialList}>
         {Object.values(config.socials).map(social => (
