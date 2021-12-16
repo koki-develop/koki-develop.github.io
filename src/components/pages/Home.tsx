e2b34e402e41c47d0c767c30f7de7fc4e0cd4217
koki-develop <kou.pg.0131@gmail.com>
2021-12-16 09:49:45 +0900

    一部修正

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -7,10 +7,8 @@ import ProfileBlock from '@/components/model/profile/ProfileBlock';
 import ProfileEmail from '@/components/model/profile/ProfileEmail';
 import SocialList from '@/components/model/social/SocialList';
 import SkillCardList from '@/components/model/skill/SkillCardList';
-import SkillsSection from '@/components/model/skill/SkillsSection';
 import WorksSection from '@/components/model/work/WorksSection';
 import HistorySection from '@/components/model/history/HistorySection';
-import ProfileContactSection from '@/components/model/profile/ProfileEmail';
 import Section from '@/components/utils/Section';
 
 export type HomeProps = {
@@ -27,17 +25,16 @@ const Home: NextPage<HomeProps> = props => {
         <SocialList socials={config.socials} />
       </Section>
 
-      <Section title='Contact'>
-        <ProfileEmail config={config} />
-      </Section>
-
       <Section title='Skills'>
         <SkillCardList config={config} />
       </Section>
 
       <WorksSection config={config} />
       <HistorySection config={config} />
-      <ProfileContactSection config={config} />
+
+      <Section title='Contact'>
+        <ProfileEmail config={config} />
+      </Section>
     </Layout>
   );
 };
