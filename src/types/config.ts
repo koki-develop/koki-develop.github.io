0216f4d9783c47e4efe957afb9a70e4093440d50
koki-develop <kou.pg.0131@gmail.com>
2021-12-16 09:02:37 +0900

    やっぱり profile.ts は types に置いておく

--- src/types/config.ts
+++ src/types/config.ts
@@ -1,4 +1,4 @@
-import { Profile } from '@/components/model/profile';
+import { Profile } from '@/types/profile';
 import { Socials } from '@/types/socials';
 import { SkillGroup } from '@/types/skillGroup';
 import { Work } from '@/types/work';
