1111ceb97faf60af037645d6b1c140934e949fce
koki-develop <kou.pg.0131@gmail.com>
2021-12-16 19:36:52 +0900

    SkillCardList で createStyles, makeStyles を使用するのをやめた

--- src/components/model/skill/SkillCardList.tsx
+++ src/components/model/skill/SkillCardList.tsx
@@ -1,33 +1,15 @@
 import React from 'react';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import createStyles from '@mui/styles/createStyles';
-import makeStyles from '@mui/styles/makeStyles';
 import { SkillGroup } from '@/types/skillGroup';
 import FadeSlideUp from '@/components/utils/FadeSlideUp';
 import SkillCard from '@/components/model/skill/SkillCard';
-
-const useStyles = makeStyles(theme =>
-  createStyles({
-    groupName: {
-      fontSize: theme.typography.body1.fontSize,
-      fontWeight: 'bold',
-      marginBottom: theme.spacing(1),
-      textAlign: 'center',
-    },
-    skillsContainer: {
-      marginBottom: theme.spacing(2),
-    },
-  }),
-);
 
 export type SkillCardListProps = {
   skillGroups: SkillGroup[];
 };
 
 const SkillCardList: React.VFC<SkillCardListProps> = React.memo(props => {
-  const classes = useStyles();
-
   const { skillGroups } = props;
 
   return (
@@ -35,9 +17,11 @@ const SkillCardList: React.VFC<SkillCardListProps> = React.memo(props => {
       {skillGroups.map(group => (
         <div key={group.name}>
           <FadeSlideUp>
-            <Typography className={classes.groupName}>{group.name}</Typography>
+            <Typography sx={{ fontWeight: 'bold', mb: 1, textAlign: 'center' }}>
+              {group.name}
+            </Typography>
           </FadeSlideUp>
-          <Grid className={classes.skillsContainer} container spacing={2}>
+          <Grid container spacing={2} sx={{ mb: 2 }}>
             {group.skills.map(skill => (
               <SkillCard key={skill.name} skill={skill} />
             ))}
