ccefadae84256409aa1a4bc1c7c590bd1661d143
koki-develop <kou.pg.0131@gmail.com>
2021-12-16 09:20:49 +0900

    SocialList コンポーネントを作成

--- src/components/model/profile/ProfileSection.tsx
+++ src/components/model/profile/ProfileSection.tsx
@@ -1,52 +1,20 @@
 import React from 'react';
-import { createStyles, makeStyles } from '@material-ui/core/styles';
 import { Config } from '@/types/config';
 import ProfileBlock from '@/components/model/profile/ProfileBlock';
+import SocialList from '@/components/model/social/SocialList';
 import Section from '@/components/utils/Section';
-import ExternalLink from '@/components/utils/ExternalLink';
-
-const useStyles = makeStyles(theme =>
-  createStyles({
-    socialList: {
-      display: 'flex',
-      justifyContent: 'center',
-    },
-    socialListItem: {
-      margin: theme.spacing(2),
-    },
-    socialIcon: {
-      height: 40,
-      width: 40,
-    },
-  }),
-);
 
 export type AboutSectionProps = {
   config: Config;
 };
 
 const AboutSection: React.VFC<AboutSectionProps> = React.memo(props => {
-  const classes = useStyles();
-
   const { config } = props;
 
   return (
     <Section title='About' hideTitle>
       <ProfileBlock config={config} />
-
-      <ul className={classes.socialList}>
-        {Object.values(config.socials).map(social => (
-          <li key={social.name} className={classes.socialListItem}>
-            <ExternalLink href={social.url}>
-              <img
-                className={classes.socialIcon}
-                src={social.imageUrl}
-                alt={social.name}
-              />
-            </ExternalLink>
-          </li>
-        ))}
-      </ul>
+      <SocialList config={config} />
     </Section>
   );
 });
---
+++ src/components/model/social/SocialList.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
+import { Config } from '@/types/config';
+import ExternalLink from '@/components/utils/ExternalLink';
+
+const useStyles = makeStyles(theme =>
+  createStyles({
+    socialList: {
+      display: 'flex',
+      justifyContent: 'center',
+    },
+    socialListItem: {
+      margin: theme.spacing(2),
+    },
+    socialIcon: {
+      height: 40,
+      width: 40,
+    },
+  }),
+);
+
+export type SocialListProps = {
+  config: Config;
+};
+
+const SocialList: React.VFC<SocialListProps> = React.memo(props => {
+  const classes = useStyles();
+
+  const { config } = props;
+
+  return (
+    <ul className={classes.socialList}>
+      {Object.values(config.socials).map(social => (
+        <li key={social.name} className={classes.socialListItem}>
+          <ExternalLink href={social.url}>
+            <img
+              className={classes.socialIcon}
+              src={social.imageUrl}
+              alt={social.name}
+            />
+          </ExternalLink>
+        </li>
+      ))}
+    </ul>
+  );
+});
+
+SocialList.displayName = 'SocialList';
+
+export default SocialList;
