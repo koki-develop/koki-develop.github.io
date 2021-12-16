423cda2958ea54948bec7c62c1fc2b9b2700e29e
koki-develop <kou.pg.0131@gmail.com>
2021-12-16 18:52:01 +0900

    ProfileEmail で createStyle, makeStyle を使用するのをやめた

--- src/components/model/profile/ProfileEmail.tsx
+++ src/components/model/profile/ProfileEmail.tsx
@@ -1,42 +1,15 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import createStyles from '@mui/styles/createStyles';
-import makeStyles from '@mui/styles/makeStyles';
 import DoneIcon from '@mui/icons-material/Done';
 import MailIcon from '@mui/icons-material/Mail';
-
-const useStyles = makeStyles(theme =>
-  createStyles({
-    buttonsContainer: {
-      alignItems: 'center',
-      display: 'flex',
-      flexFlow: 'column',
-    },
-    mailButton: {
-      fontWeight: 'bold',
-      marginBottom: theme.spacing(0.8),
-      '&:hover': {
-        opacity: 1,
-      },
-    },
-    copyButton: {
-      backgroundColor: 'transparent',
-      color: theme.palette.primary.contrastText,
-    },
-    copiedIcon: {
-      color: theme.palette.success.main,
-    },
-  }),
-);
 
 export type ProfileEmailProps = {
   email: string;
 };
 
 const ProfileEmail: React.VFC<ProfileEmailProps> = React.memo(props => {
-  const classes = useStyles();
-
   const { email } = props;
 
   const [showCopiedMessage, setShowCopiedMessage] = useState<boolean>(false);
@@ -47,45 +20,50 @@ const ProfileEmail: React.VFC<ProfileEmailProps> = React.memo(props => {
   }, [showCopiedMessage]);
 
   useEffect(() => {
-    if (showCopiedMessage) {
-      const timeoutId = setTimeout(() => {
-        setShowCopiedMessage(false);
-      }, 1000);
-      return () => {
-        clearTimeout(timeoutId);
-      };
-    }
+    if (!showCopiedMessage) return;
+
+    const timeoutId = setTimeout(() => {
+      setShowCopiedMessage(false);
+    }, 1000);
+    return () => {
+      clearTimeout(timeoutId);
+    };
   }, [showCopiedMessage]);
 
   return (
-    <div className={classes.buttonsContainer}>
-      <div>
+    <Box sx={{ alignItems: 'center', display: 'flex', flexFlow: 'column' }}>
+      <Box>
         <Button
-          className={classes.mailButton}
           startIcon={<MailIcon />}
           href={`mailto:${email}`}
           target='_blank'
           rel='noreferrer noopener'
+          sx={{
+            fontWeight: 'bold',
+            marginBottom: 0.8,
+            '&:hover': {
+              opacity: 1,
+            },
+          }}
         >
           {email}
         </Button>
-      </div>
-      <div>
+      </Box>
+      <Box>
         <CopyToClipboard text={email} onCopy={handleCopyEmail}>
           <Button
-            className={classes.copyButton}
             variant='text'
-            startIcon={
-              showCopiedMessage ? (
-                <DoneIcon className={classes.copiedIcon} />
-              ) : null
-            }
+            startIcon={showCopiedMessage ? <DoneIcon color='success' /> : null}
+            sx={{
+              backgroundColor: 'transparent',
+              color: theme => theme.palette.primary.contrastText,
+            }}
           >
             {showCopiedMessage ? 'コピーしました' : 'クリップボードにコピー'}
           </Button>
         </CopyToClipboard>
-      </div>
-    </div>
+      </Box>
+    </Box>
   );
 });
 
