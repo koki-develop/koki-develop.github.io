00ae526a38fe6f60fe81b2f0b720abf492af162c
koki-develop <kou.pg.0131@gmail.com>
2021-12-18 19:19:24 +0900

    Section で useStyles を使用するのをやめた

--- src/components/utils/Section.tsx
+++ src/components/utils/Section.tsx
@@ -1,23 +1,8 @@
 import React from 'react';
+import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Divider from '@mui/material/Divider';
 import Typography from '@mui/material/Typography';
-import createStyles from '@mui/styles/createStyles';
-import makeStyles from '@mui/styles/makeStyles';
-
-const useStyles = makeStyles(theme =>
-  createStyles({
-    section: {
-      margin: theme.spacing(3),
-    },
-    title: {
-      fontSize: 28,
-      fontWeight: 'bold',
-      marginBottom: theme.spacing(2),
-      textAlign: 'center',
-    },
-  }),
-);
 
 export type SectionProps = {
   children: React.ReactNode;
@@ -27,21 +12,28 @@ export type SectionProps = {
 };
 
 const Section: React.VFC<SectionProps> = React.memo(props => {
-  const classes = useStyles();
-
   const { children, title, hideTitle, disablePadding } = props;
 
   return (
     <div>
       {title && <Divider id={title} />}
-      <section className={classes.section}>
+      <Box component='section' sx={{ m: 3 }}>
         {title && !hideTitle && (
-          <Typography className={classes.title}>{title}</Typography>
+          <Typography
+            sx={{
+              fontSize: 28,
+              fontWeight: 'bold',
+              mb: 2,
+              textAlign: 'center',
+            }}
+          >
+            {title}
+          </Typography>
         )}
         <Container disableGutters={disablePadding} maxWidth='md'>
           {children}
         </Container>
-      </section>
+      </Box>
     </div>
   );
 });
