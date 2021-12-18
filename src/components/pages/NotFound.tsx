b8b5bdedc93968d211b3cfa15a4f304a54a209b6
koki-develop <kou.pg.0131@gmail.com>
2021-12-18 19:13:45 +0900

    NotFound で useStyles を使用するのをやめた

--- src/components/pages/NotFound.tsx
+++ src/components/pages/NotFound.tsx
@@ -1,42 +1,33 @@
 import React from 'react';
 import { NextPage, GetStaticProps } from 'next';
+import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
-import createStyles from '@mui/styles/createStyles';
-import makeStyles from '@mui/styles/makeStyles';
 import { ConfigLoader } from '@/lib/configLoader';
 import { Config } from '@/types/config';
 import Layout from '@/components/Layout';
-
-const useStyles = makeStyles(theme =>
-  createStyles({
-    messageContainer: {
-      marginTop: theme.spacing(2),
-      marginBottom: theme.spacing(2),
-    },
-    message: {
-      fontSize: theme.typography.h5.fontSize,
-      fontWeight: 'bold',
-      textAlign: 'center',
-    },
-  }),
-);
 
 export type NotFoundProps = {
   config: Config;
 };
 
 const NotFound: NextPage<NotFoundProps> = props => {
-  const classes = useStyles();
-
   const { config } = props;
 
   const message = 'お探しのページは見つかりませんでした';
 
   return (
     <Layout config={config} title={message} hideMenu>
-      <div className={classes.messageContainer}>
-        <Typography className={classes.message}>{message}</Typography>
-      </div>
+      <Box sx={{ my: 2 }}>
+        <Typography
+          sx={theme => ({
+            fontSize: theme.typography.h5.fontSize,
+            fontWeight: 'bold',
+            textAlign: 'center',
+          })}
+        >
+          {message}
+        </Typography>
+      </Box>
     </Layout>
   );
 };
