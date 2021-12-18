844900fa06d63e42f6ba51cb872bca74daa88a42
koki-develop <kou.pg.0131@gmail.com>
2021-12-18 19:11:53 +0900

    HistoryTimeline で useStyles を使用するのをやめた

--- src/components/model/history/HistoryTimeline.tsx
+++ src/components/model/history/HistoryTimeline.tsx
@@ -8,68 +8,39 @@ import TimelineConnector from '@mui/lab/TimelineConnector';
 import TimelineDot from '@mui/lab/TimelineDot';
 import TimelineItem from '@mui/lab/TimelineItem';
 import TimelineSeparator from '@mui/lab/TimelineSeparator';
-import createStyles from '@mui/styles/createStyles';
-import makeStyles from '@mui/styles/makeStyles';
 import { History } from '@/types/history';
 import FadeSlideUp from '@/components/utils/FadeSlideUp';
-
-const useStyles = makeStyles(theme =>
-  createStyles({
-    timeline: {
-      padding: 0,
-    },
-    timelineItem: {
-      [theme.breakpoints.down('md')]: {
-        flexDirection: 'row !important',
-        '&::before': {
-          flex: 0,
-        },
-      },
-      [theme.breakpoints.down('sm')]: {
-        '&::before': {
-          padding: '6px 8px',
-        },
-      },
-    },
-    period: {
-      fontSize: theme.typography.caption.fontSize,
-      [theme.breakpoints.down('md')]: {
-        textAlign: 'left !important' as 'left',
-      },
-    },
-    title: {
-      fontSize: theme.typography.body1.fontSize,
-      fontWeight: 'bold',
-      marginTop: theme.spacing(1),
-      marginBottom: theme.spacing(1),
-      [theme.breakpoints.down('md')]: {
-        textAlign: 'left !important' as 'left',
-      },
-    },
-    description: {
-      fontSize: theme.typography.body2.fontSize,
-      textAlign: 'left !important' as 'left',
-    },
-  }),
-);
 
 export type HistoryTimelineProps = {
   histories: History[];
 };
 
 const HistoryTimeline: React.VFC<HistoryTimelineProps> = React.memo(props => {
-  const classes = useStyles();
-
   const { histories } = props;
 
   return (
-    <Timeline className={classes.timeline} position='alternate'>
+    <Timeline position='alternate' sx={{ p: 0 }}>
       {histories.map((history, i) => (
-        <TimelineItem className={classes.timelineItem} key={history.title}>
+        <TimelineItem
+          key={history.title}
+          sx={theme => ({
+            [theme.breakpoints.down('md')]: {
+              flexDirection: 'row !important',
+              '&::before': {
+                flex: 0,
+              },
+            },
+            [theme.breakpoints.down('sm')]: {
+              '&::before': {
+                padding: '6px 8px',
+              },
+            },
+          })}
+        >
           <TimelineSeparator>
             <TimelineDot
-              color={history.isActive ? 'secondary' : undefined}
               variant={history.isActive ? undefined : 'outlined'}
+              sx={{ backgroundColor: history.isActive ? 'red' : 'white' }}
             />
             {i !== histories.length - 1 && <TimelineConnector />}
           </TimelineSeparator>
@@ -77,13 +48,34 @@ const HistoryTimeline: React.VFC<HistoryTimelineProps> = React.memo(props => {
             <FadeSlideUp>
               <Card>
                 <CardContent>
-                  <Typography className={classes.period}>
+                  <Typography
+                    sx={theme => ({
+                      fontSize: theme.typography.caption.fontSize,
+                      [theme.breakpoints.down('md')]: {
+                        textAlign: 'left !important' as 'left',
+                      },
+                    })}
+                  >
                     {history.from} - {history.to}
                   </Typography>
-                  <Typography className={classes.title}>
+                  <Typography
+                    sx={theme => ({
+                      fontSize: theme.typography.body1.fontSize,
+                      fontWeight: 'bold',
+                      my: 1,
+                      [theme.breakpoints.down('md')]: {
+                        textAlign: 'left !important' as 'left',
+                      },
+                    })}
+                  >
                     {history.title}
                   </Typography>
-                  <Typography className={classes.description}>
+                  <Typography
+                    sx={theme => ({
+                      fontSize: theme.typography.body2.fontSize,
+                      textAlign: 'left !important' as 'left',
+                    })}
+                  >
                     {history.description}
                   </Typography>
                 </CardContent>
