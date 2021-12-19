b06c862974c132fe634150b38525f9af67bf695f
koki-develop <kou.pg.0131@gmail.com>
2021-12-19 21:34:40 +0900

    WorkCardList をリファクタ

---
+++ src/components/model/work/WorkCard.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardContent from '@mui/material/CardContent';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import urlJoin from 'url-join';
+import { Social } from '@/types/socials';
+import ExternalLink from '@/components/utils/ExternalLink';
+import SkillAvatarGroup from '@/components/model/skill/SkillAvatarGroup';
+import { Work } from '@/types/work';
+
+export type WorkCardProps = {
+  githubSocial: Social;
+  work: Work;
+};
+
+const WorkCard: React.VFC<WorkCardProps> = React.memo(props => {
+  const { work, githubSocial } = props;
+
+  return (
+    <Card raised>
+      {work.hasImage && (
+        <CardActionArea>
+          <ExternalLink href={work.url}>
+            <CardMedia
+              image={urlJoin('/images/works', `${work.name}.png`)}
+              sx={{ height: { xs: 150, sm: 230, md: 280, lg: 300 } }}
+            />
+          </ExternalLink>
+        </CardActionArea>
+      )}
+      <CardHeader
+        title={
+          <ExternalLink
+            href={work.url}
+            sx={{
+              color: theme => theme.palette.primary.contrastText,
+              fontWeight: 'bold',
+            }}
+          >
+            {work.name}
+          </ExternalLink>
+        }
+        sx={{ pb: 0 }}
+      />
+      <CardContent sx={{ pt: 0 }}>
+        <SkillAvatarGroup skills={work.skills} />
+        <Typography
+          sx={{
+            fontSize: theme => theme.typography.body2.fontSize,
+            mb: 1,
+          }}
+        >
+          {work.description}
+        </Typography>
+        <ExternalLink
+          href={urlJoin(
+            'https://github.com',
+            githubSocial.username,
+            work.repository,
+          )}
+          sx={{ color: theme => theme.palette.primary.contrastText }}
+        >
+          View on GitHub
+        </ExternalLink>
+      </CardContent>
+    </Card>
+  );
+});
+
+WorkCard.displayName = 'WorkCard';
+
+export default WorkCard;
--- src/components/model/work/WorkCardList.tsx
+++ src/components/model/work/WorkCardList.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
 import Button from '@mui/material/Button';
-import Card from '@mui/material/Card';
-import CardActionArea from '@mui/material/CardActionArea';
-import CardContent from '@mui/material/CardContent';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
 import Grid from '@mui/material/Grid';
-import Typography from '@mui/material/Typography';
 import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
-import urlJoin from 'url-join';
 import { Social } from '@/types/socials';
-import ExternalLink from '@/components/utils/ExternalLink';
 import FadeSlideUp from '@/components/utils/FadeSlideUp';
-import SkillAvatarGroup from '@/components/model/skill/SkillAvatarGroup';
+import WorkCard from '@/components/model/work/WorkCard';
 import { Work } from '@/types/work';
 
 export type WorkCardListProps = {
@@ -28,54 +20,7 @@ const WorkCardList: React.VFC<WorkCardListProps> = React.memo(props => {
       {works.map(work => (
         <Grid key={work.name} item xs={12}>
           <FadeSlideUp>
-            <Card raised>
-              {work.hasImage && (
-                <CardActionArea>
-                  <ExternalLink href={work.url}>
-                    <CardMedia
-                      image={urlJoin('/images/works', `${work.name}.png`)}
-                      sx={{ height: { xs: 150, sm: 230, md: 280, lg: 300 } }}
-                    />
-                  </ExternalLink>
-                </CardActionArea>
-              )}
-              <CardHeader
-                title={
-                  <ExternalLink
-                    href={work.url}
-                    sx={{
-                      color: theme => theme.palette.primary.contrastText,
-                      fontWeight: 'bold',
-                      '&:hover': { opacity: 1 },
-                    }}
-                  >
-                    {work.name}
-                  </ExternalLink>
-                }
-                sx={{ pb: 0 }}
-              />
-              <CardContent sx={{ pt: 0 }}>
-                <SkillAvatarGroup skills={work.skills} />
-                <Typography
-                  sx={{
-                    fontSize: theme => theme.typography.body2.fontSize,
-                    mb: 1,
-                  }}
-                >
-                  {work.description}
-                </Typography>
-                <ExternalLink
-                  href={urlJoin(
-                    'https://github.com',
-                    githubSocial.username,
-                    work.repository,
-                  )}
-                  sx={{ color: theme => theme.palette.primary.contrastText }}
-                >
-                  View on GitHub
-                </ExternalLink>
-              </CardContent>
-            </Card>
+            <WorkCard work={work} githubSocial={githubSocial} />
           </FadeSlideUp>
         </Grid>
       ))}
