763aa992dd6474655fa29114f0a793e519428ab0
koki-develop <kou.pg.0131@gmail.com>
2021-12-19 21:38:25 +0900

    App コンポーネントを components/ 配下に作成

---
+++ src/components/App/App.tsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+import { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+
+const App: React.VFC<AppProps> = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (process.env.NEXT_PUBLIC_ENV === 'production') {
+      window.gtag('config', process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID, {
+        page_path: router.pathname,
+      });
+    }
+  }, [router.pathname]);
+
+  return <Component {...pageProps} />;
+};
+
+export default App;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,4 @@
-import React, { useEffect } from 'react';
-import { AppProps } from 'next/app';
-import { useRouter } from 'next/router';
+import App from '@/components/App';
 import '@/styles/global.scss';
-
-const App: React.VFC<AppProps> = ({ Component, pageProps }: AppProps) => {
-  const router = useRouter();
-
-  useEffect(() => {
-    if (process.env.NEXT_PUBLIC_ENV === 'production') {
-      window.gtag('config', process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID, {
-        page_path: router.pathname,
-      });
-    }
-  }, [router.pathname]);
-
-  return <Component {...pageProps} />;
-};
 
 export default App;
