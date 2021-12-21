fd4687348c852ca66ce590d18b06624e0c77fffd
koki-develop <kou.pg.0131@gmail.com>
2021-12-21 09:37:25 +0900

    head 内の設定を App コンポーネントに移動

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,51 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import { BrowserRouter } from 'react-router-dom';
+import urlJoin from 'url-join';
 import AppRoutes from '@/components/App/AppRoutes';
+import { config } from '@/config';
 
 const App: React.VFC = React.memo(() => {
   return (
     <BrowserRouter>
+      <Helmet>
+        {import.meta.env.VITE_ENV === 'production' && (
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${
+                import.meta.env.VITE_GA_MEASUREMENT_ID
+              }`}
+            ></script>
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+
+                  gtag('config', '${import.meta.env.VITE_GA_MEASUREMENT_ID}');
+                `,
+              }}
+            ></script>
+          </>
+        )}
+        <meta property='og:site_name' content={config.profile.name} />
+        <meta name='description' content={config.profile.description} />
+        <meta property='og:description' content={config.profile.description} />
+        <meta property='og:url' content={config.url} />
+        <meta
+          property='og:image'
+          content={urlJoin(config.url, 'images/profile.jpg')}
+        />
+        <meta property='og:type' content='website' />
+        <meta property='og:locale' content='ja_JP' />
+        <meta property='twitter:card' content='summary' />
+        <meta
+          property='twitter:site'
+          content={`@${config.socials.twitter.username}`}
+        />
+      </Helmet>
       <AppRoutes />
     </BrowserRouter>
   );
