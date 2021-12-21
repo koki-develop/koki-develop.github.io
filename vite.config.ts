1b2242df5b38ded070524c4da8cde54acef8095a
koki-develop <kou.pg.0131@gmail.com>
2021-12-21 10:47:05 +0900

    minifyHtml を追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import { injectHtml } from 'vite-plugin-html';
+import { injectHtml, minifyHtml } from 'vite-plugin-html';
 import * as path from 'path';
 
 export default defineConfig(env => {
@@ -15,6 +15,6 @@ export default defineConfig(env => {
     server: {
       open: true,
     },
-    plugins: [react(), injectHtml({ data: { env: loadedEnv } })],
+    plugins: [react(), minifyHtml(), injectHtml({ data: { env: loadedEnv } })],
   };
 });
