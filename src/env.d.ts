e6b1e3a043086ebd34d1922d389106680c5de72d
koki-develop <kou.pg.0131@gmail.com>
2021-12-21 10:16:26 +0900

    VITE_ENV の型を修正

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,7 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_ENV: string;
+  readonly VITE_ENV: 'development' | 'production';
   readonly VITE_GA_MEASUREMENT_ID: string;
 }
 
