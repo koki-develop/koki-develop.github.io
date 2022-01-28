a446aaf1f6d17788384b106496134bd59ce370bf
koki-develop <kou.pg.0131@gmail.com>
2022-01-28 22:35:49 +0900

    notesLoader を作成

---
+++ src/lib/notesLoader.ts
@@ -0,0 +1,32 @@
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+import { Note } from '@/types/note';
+
+export class NotesLoader {
+  public static load(slug: string): Note {
+    const content = fs.readFileSync(
+      path.join(this._getNotesDirectoryPath(), `${slug}.md`),
+    );
+    const mattered = matter(content);
+
+    return {
+      slug,
+      content: mattered.content,
+      ...mattered.data,
+    } as Note;
+  }
+
+  public static loadAll(): Note[] {
+    const filenames = fs.readdirSync(this._getNotesDirectoryPath());
+
+    return filenames.map(filename => {
+      const slug = path.parse(filename).name;
+      return this.load(slug);
+    });
+  }
+
+  private static _getNotesDirectoryPath(): string {
+    return path.join(process.cwd(), 'src/notes');
+  }
+}
