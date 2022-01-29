bced6273aa20593edfb99c115168089d96cc8766
koki-develop <kou.pg.0131@gmail.com>
2022-01-29 23:45:10 +0900

    NoteTagChip から ChipProps を削除

--- src/components/model/note/NoteTagChip.tsx
+++ src/components/model/note/NoteTagChip.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import Chip, { ChipProps } from '@mui/material/Chip';
+import Chip from '@mui/material/Chip';
 import urlJoin from 'url-join';
 
-export type NoteTagChipProps = ChipProps & {
+export type NoteTagChipProps = {
   tag: string;
 };
 
 const NoteTagChip: React.VFC<NoteTagChipProps> = React.memo(props => {
-  const { tag, ...chipProps } = props;
+  const { tag } = props;
 
   return (
     <Chip
-      {...chipProps}
       sx={{
         backgroundColor: theme => theme.palette.primary.main,
         border: '1px solid',
         borderColor: 'divider',
         cursor: 'inherit',
-        ...chipProps.sx,
       }}
       avatar={
         <img
