1e041df83574f49d5ea6257b92a397c6d7d64270
koki-develop <kou.pg.0131@gmail.com>
2022-01-29 10:17:27 +0900

    ノートの内容を Paper に入れるようにした

--- src/components/pages/Note/Note.tsx
+++ src/components/pages/Note/Note.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
 import Layout from '@/components/Layout';
 import { Note } from '@/types/note';
 import 'zenn-content-css';
@@ -14,11 +16,19 @@ const NotePage: React.VFC<NotePageProps> = React.memo(props => {
 
   return (
     <Layout title={note.title} hideSiteName>
-      <Container maxWidth='md'>
-        <Box
-          className='znc'
-          dangerouslySetInnerHTML={{ __html: note.content }}
-        />
+      <Container maxWidth='md' disableGutters sx={{ pt: 4 }}>
+        <Paper square elevation={2} sx={{ px: { xs: 2, md: 5 }, py: 2 }}>
+          <Box sx={{ mb: 3 }}>
+            <Typography variant='h4' sx={{ fontWeight: 'bold', mb: 2 }}>
+              {note.title}
+            </Typography>
+          </Box>
+
+          <Box
+            className='znc'
+            dangerouslySetInnerHTML={{ __html: note.content }}
+          />
+        </Paper>
       </Container>
     </Layout>
   );
