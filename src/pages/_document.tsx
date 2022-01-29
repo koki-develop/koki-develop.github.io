f6638bea6f615e1701d94e70c414be8c367edd2e
koki-develop <kou.pg.0131@gmail.com>
2022-01-29 19:46:37 +0900

    Document.getInitialProps を削除

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -42,23 +42,3 @@ export default class Document extends NextDocument {
     );
   }
 }
-
-Document.getInitialProps = async (ctx: DocumentContext) => {
-  const sheets = new ServerStyleSheets();
-  const originalRenderPage = ctx.renderPage;
-
-  ctx.renderPage = () =>
-    originalRenderPage({
-      enhanceApp: App => props => sheets.collect(<App {...props} />),
-    });
-
-  const initialProps = await NextDocument.getInitialProps(ctx);
-
-  return {
-    ...initialProps,
-    styles: [
-      ...React.Children.toArray(initialProps.styles),
-      sheets.getStyleElement(),
-    ],
-  };
-};
