2704883bf76fd48cb8d1ee98e8c7408718c146b7
koki-develop <kou.pg.0131@gmail.com>
2022-01-29 10:39:11 +0900

    タグを表示するようにした

--- src/components/pages/Note/Note.tsx
+++ src/components/pages/Note/Note.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import Box from '@mui/material/Box';
+import Chip from '@mui/material/Chip';
 import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper';
+import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+import urlJoin from 'url-join';
 import Layout from '@/components/Layout';
 import { Note } from '@/types/note';
 import 'zenn-content-css';
@@ -22,6 +25,26 @@ const NotePage: React.VFC<NotePageProps> = React.memo(props => {
             <Typography variant='h4' sx={{ fontWeight: 'bold', mb: 2 }}>
               {note.title}
             </Typography>
+            <Stack direction='row' spacing={1}>
+              {note.tags.map(tag => (
+                <Chip
+                  key={tag}
+                  sx={{
+                    backgroundColor: theme => theme.palette.primary.main,
+                    border: '1px solid',
+                    borderColor: 'divider',
+                  }}
+                  avatar={
+                    <img
+                      src={urlJoin('/images/icons', `${tag}.svg`)}
+                      style={{ maxHeight: '50%', maxWidth: '50%' }}
+                      alt={tag}
+                    />
+                  }
+                  label={tag}
+                />
+              ))}
+            </Stack>
           </Box>
 
           <Box
