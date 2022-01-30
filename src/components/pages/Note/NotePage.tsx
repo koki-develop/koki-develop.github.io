0f06cccbb4fb3b6de81da18dad1e4efe41b2cf88
koki-develop <kou.pg.0131@gmail.com>
2022-01-30 21:37:16 +0900

    作成日時を表示するようにした

--- src/components/pages/Note/NotePage.tsx
+++ src/components/pages/Note/NotePage.tsx
@@ -4,6 +4,8 @@ import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import CreateIcon from '@mui/icons-material/Create';
+import UpdateIcon from '@mui/icons-material/Update';
 import 'zenn-content-css';
 import Layout from '@/components/Layout';
 import NoteTagChipList from '@/components/model/note/NoteTagChipList';
@@ -37,12 +39,39 @@ const NotePage: React.VFC<NotePageProps> = React.memo(props => {
 
         <Paper square sx={{ px: { xs: 2, md: 5 }, py: 2 }}>
           <Box sx={{ mb: 5 }}>
-            <Typography
-              variant='body2'
-              sx={{ color: theme => theme.palette.text.secondary, mb: 1 }}
-            >
-              <Time datetime={note.updatedAt} /> に更新
-            </Typography>
+            <Box sx={{ display: 'flex' }}>
+              <Typography
+                component='span'
+                variant='body2'
+                sx={{
+                  color: theme => theme.palette.text.secondary,
+                  display: 'flex',
+                  mb: 1,
+                }}
+              >
+                <CreateIcon fontSize='small' sx={{ mr: 0.5 }} />
+                <Time datetime={note.createdAt} /> に作成
+              </Typography>
+              {note.createdAt !== note.updatedAt && (
+                <Typography
+                  component='span'
+                  variant='body2'
+                  sx={{
+                    color: theme => theme.palette.text.secondary,
+                    display: {
+                      xs: 'none',
+                      sm: 'flex',
+                    },
+                    ml: 2,
+                    mb: 1,
+                  }}
+                >
+                  <UpdateIcon fontSize='small' sx={{ mr: 0.5 }} />
+                  <Time datetime={note.updatedAt} /> に更新
+                </Typography>
+              )}
+            </Box>
+
             <Typography
               component='h1'
               variant='h4'
