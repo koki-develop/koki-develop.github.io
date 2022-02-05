c4e66628e0a2893534833dbd9a7755c36f5013da
koki-develop <kou.pg.0131@gmail.com>
2022-02-05 18:10:56 +0900

    アクティブな見出しを強調できるように実装

--- src/components/model/note/NoteTableOfContents.tsx
+++ src/components/model/note/NoteTableOfContents.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useMemo, useState } from 'react';
 import Box from '@mui/material/Box';
 import { TableOfContentsItem } from '@/types/note';
 import NoteTableOfContentsItem from '@/components/model/note/NoteTableOfContentsItem';
@@ -11,10 +11,30 @@ const NoteTableOfContents: React.VFC<NoteTableOfContentsProps> = React.memo(
   props => {
     const { items } = props;
 
+    const [activeItemIds, setActiveItemIds] = useState<string[]>([]);
+    const activeItemId = useMemo(() => {
+      if (activeItemIds.length === 0) return null;
+      return activeItemIds.slice(-1)[0];
+    }, [activeItemIds]);
+
+    const handleActiveItem = useCallback((item: TableOfContentsItem) => {
+      setActiveItemIds(prev => [...prev, item.id]);
+    }, []);
+
+    const handleInactiveItem = useCallback((item: TableOfContentsItem) => {
+      setActiveItemIds(prev => prev.filter(id => id !== item.id));
+    }, []);
+
     return (
       <Box>
         {items.map(item => (
-          <NoteTableOfContentsItem key={item.id} item={item} />
+          <NoteTableOfContentsItem
+            key={item.id}
+            item={item}
+            active={item.id === activeItemId}
+            onActive={handleActiveItem}
+            onInactive={handleInactiveItem}
+          />
         ))}
       </Box>
     );
--- src/components/model/note/NoteTableOfContentsItem.tsx
+++ src/components/model/note/NoteTableOfContentsItem.tsx
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, { useEffect, useMemo } from 'react';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
 import Box from '@mui/material/Box';
 import { Link } from 'react-scroll';
 import { TableOfContentsItem } from '@/types/note';
 
 export type NoteTableOfContentsItemProps = {
   item: TableOfContentsItem;
+  active: boolean;
+  onActive: (item: TableOfContentsItem) => void;
+  onInactive: (item: TableOfContentsItem) => void;
 };
 
 const NoteTableOfContentsItem: React.VFC<NoteTableOfContentsItemProps> =
   React.memo(props => {
-    const { item } = props;
+    const { item, active, onActive, onInactive } = props;
+
+    const offset = useMemo(() => {
+      if (typeof window === 'undefined') return 0;
+      const elm = document.getElementById(item.id);
+      return (
+        window.scrollY + elm.getBoundingClientRect().top - elm.clientHeight
+      );
+    }, [item.id]);
+
+    const inView = useScrollTrigger({
+      disableHysteresis: true,
+      threshold: offset,
+    });
+
+    useEffect(() => {
+      if (inView) {
+        onActive(item);
+      } else {
+        onInactive(item);
+      }
+    }, [inView, item, onActive, onInactive]);
 
     return (
       <Box key={item.id}>
         <Link
+          style={{ color: active ? 'red' : null }}
           href={`#${item.id}`}
           to={item.id}
           smooth
