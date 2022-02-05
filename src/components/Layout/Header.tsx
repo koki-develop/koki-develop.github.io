a38b3bfd216aae6fdac521419fe66a3e02cddc48
koki-develop <kou.pg.0131@gmail.com>
2022-02-05 19:24:50 +0900

    画面が小さい場合はヘッダーを上部に固定するようにした

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -4,13 +4,18 @@ import Avatar from '@mui/material/Avatar';
 import Container from '@mui/material/Container';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import { useTheme } from '@mui/material/styles';
+import { useMediaQuery } from '@mui/material';
 import Link from '@/components/utils/Link';
 import { Routes } from '@/routes';
 import { config } from '@/config';
 
 const Header: React.VFC = React.memo(() => {
+  const theme = useTheme();
+  const isMdDown = useMediaQuery(theme.breakpoints.down('md'));
+
   return (
-    <AppBar position='static'>
+    <AppBar position={isMdDown ? 'sticky' : 'static'}>
       <Container maxWidth='lg'>
         <Toolbar>
           <Link
