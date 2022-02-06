ed854b67994fbbe4b838a8edaaef7833fce382bc
koki-develop <kou.pg.0131@gmail.com>
2022-02-06 22:03:39 +0900

    note.description を optional な値にした

--- src/types/note.ts
+++ src/types/note.ts
@@ -1,7 +1,7 @@
 export type Note = {
   slug: string;
   title: string;
-  description: string;
+  description?: string;
   content: string;
   tags: string[];
   tableOfContents: TableOfContentsItem[];
