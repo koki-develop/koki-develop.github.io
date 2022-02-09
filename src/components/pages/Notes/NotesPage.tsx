79a83701066193fe61a43404bf509b0f44e513bb
koki-develop <kou.pg.0131@gmail.com>
2022-02-10 08:34:14 +0900

    キーワード入力を設置

--- src/components/pages/Notes/NotesPage.tsx
+++ src/components/pages/Notes/NotesPage.tsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
+import IconButton from '@mui/material/IconButton';
+import InputAdornment from '@mui/material/InputAdornment';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
+import SearchIcon from '@mui/icons-material/Search';
 import Layout from '@/components/Layout';
 import Meta from '@/components/utils/Meta';
 import NoteCardList from '@/components/model/note/NoteCardList';
@@ -18,8 +25,39 @@ const NotesPage: React.VFC<NotesPageProps> = React.memo(props => {
     <Layout>
       <Meta title='Notes' />
 
-      <Container sx={{ mt: 2 }}>
-        <NoteCardList stacked notes={notes} />
+      <Container maxWidth='lg' sx={{ mt: 2 }}>
+        <Grid container spacing={2}>
+          <Grid
+            item
+            xs={0}
+            md={3}
+            sx={{
+              display: { xs: 'none', md: 'block' },
+            }}
+          >
+            <Paper sx={{ p: 2 }}>
+              <Box>
+                <TextField
+                  fullWidth
+                  label='キーワード'
+                  InputProps={{
+                    sx: { pr: 1 },
+                    endAdornment: (
+                      <InputAdornment position='end'>
+                        <IconButton>
+                          <SearchIcon />
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </Box>
+            </Paper>
+          </Grid>
+          <Grid item xs={12} md={9}>
+            <NoteCardList stacked notes={notes} />
+          </Grid>
+        </Grid>
       </Container>
     </Layout>
   );
