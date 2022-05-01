23b2f133c3d929a81eac9d04071b90bbf89f73bc
koki-develop <kou.pg.0131@gmail.com>
2022-05-01 19:19:17 +0900

    href, external は LinkProps から Pick するようにした

--- src/components/Layout/FooterLinkListItem.tsx
+++ src/components/Layout/FooterLinkListItem.tsx
@@ -1,14 +1,11 @@
 import { useTheme } from '@mui/material';
 import Box from '@mui/material/Box';
 import React from 'react';
-import Link from '@/components/utils/Link';
+import Link, { LinkProps } from '@/components/utils/Link';
 
 export type LinkListItemProps = {
   children: React.ReactNode;
-
-  href: string;
-  external?: boolean;
-};
+} & Pick<LinkProps, 'href' | 'external'>;
 
 const FooterLinkListItem: React.VFC<LinkListItemProps> = React.memo(props => {
   const { children, href, external } = props;
