576021ca39a664187d78fd5e130e9573c25f5404
koki-develop <kou.pg.0131@gmail.com>
2022-05-01 18:50:19 +0900

    generateReadme タスクを修正

--- src/tasks/generateReadme.ts
+++ src/tasks/generateReadme.ts
@@ -1,11 +1,9 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as ejs from 'ejs';
-import { ConfigLoader } from '@/lib/configLoader';
+import { config } from '@/config';
 
 (async () => {
-  const config = ConfigLoader.load();
-
   const template = fs
     .readFileSync(path.join(process.cwd(), 'README.template.md'))
     .toString();
