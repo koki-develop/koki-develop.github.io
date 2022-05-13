50501e99fccaa88e75947d1b353a144099d953cb
koki-develop <kou.pg.0131@gmail.com>
2022-05-13 18:22:59 +0900

    section コンポーネントを使うようにした

--- src/components/utils/Section.tsx
+++ src/components/utils/Section.tsx
@@ -10,13 +10,13 @@ const Section: React.VFC<SectionProps> = React.memo(props => {
   const { children, title } = props;
 
   return (
-    <div className='mb-2'>
+    <section className='mb-2'>
       <div className='mb-2 border-t' />
       <Container>
         <h2 className='mb-2 text-center text-3xl font-bold'>{title}</h2>
         <div>{children}</div>
       </Container>
-    </div>
+    </section>
   );
 });
 
