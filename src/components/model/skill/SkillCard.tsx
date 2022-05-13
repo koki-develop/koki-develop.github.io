05e4b5d09541d67ece21321ee5cd1b2895cb7e22
koki-develop <kou.pg.0131@gmail.com>
2022-05-13 21:05:08 +0900

    SkillCard コンポーネントに Card コンポーネントを適用

--- src/components/model/skill/SkillCard.tsx
+++ src/components/model/skill/SkillCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import urlJoin from 'url-join';
+import Card from '@/components/utils/Card';
 import Link from '@/components/utils/Link';
 import { Skill } from '@/types/skill';
 
@@ -11,20 +12,16 @@ const SkillCard: React.VFC<SkillCardProps> = React.memo(props => {
   const { skill } = props;
 
   return (
-    <div>
-      <Link external href={skill.url}>
-        <div className='rounded bg-white shadow transition hover:bg-gray-100'>
-          <div className='flex flex-col items-center p-2 md:py-3'>
-            <img
-              className='mb-1 h-12 w-12 sm:h-14 sm:h-14 md:h-16 md:w-16'
-              src={urlJoin('/images/icons', `${skill.name}.svg`)}
-              alt={skill.name}
-            />
-            <p className='text-sm font-bold'>{props.skill.name}</p>
-          </div>
-        </div>
-      </Link>
-    </div>
+    <Link external href={skill.url}>
+      <Card className='flex flex-col items-center p-2 py-3 transition hover:bg-gray-100'>
+        <img
+          className='mb-1 h-12 w-12 sm:h-14 sm:h-14 md:h-16 md:w-16'
+          src={urlJoin('/images/icons', `${skill.name}.svg`)}
+          alt={skill.name}
+        />
+        <p className='text-sm font-bold'>{props.skill.name}</p>
+      </Card>
+    </Link>
   );
 });
 
