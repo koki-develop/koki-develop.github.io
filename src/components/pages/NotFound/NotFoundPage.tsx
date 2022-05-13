716679428533a5b0398dfef94874bf4a87367acc
koki-develop <kou.pg.0131@gmail.com>
2022-05-13 21:24:31 +0900

    NotFoundPage に tailwindcss を適用

--- src/components/pages/NotFound/NotFoundPage.tsx
+++ src/components/pages/NotFound/NotFoundPage.tsx
@@ -1,6 +1,3 @@
-import { useTheme } from '@mui/material';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
 import React from 'react';
 import Meta from '@/components/utils/Meta';
 import Layout from '@/components/Layout';
@@ -8,22 +5,12 @@ import Layout from '@/components/Layout';
 const NotFoundPage: React.VFC = React.memo(() => {
   const message = 'お探しのページは見つかりませんでした';
 
-  const theme = useTheme();
-
   return (
     <Layout>
       <Meta title={message} />
-      <Box sx={{ my: 2 }}>
-        <Typography
-          sx={{
-            fontSize: theme.typography.h5.fontSize,
-            fontWeight: 'bold',
-            textAlign: 'center',
-          }}
-        >
-          {message}
-        </Typography>
-      </Box>
+      <div className='my-4'>
+        <p className='text-center text-lg font-bold'>{message}</p>
+      </div>
     </Layout>
   );
 });
