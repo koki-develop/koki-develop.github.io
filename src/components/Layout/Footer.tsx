7f3e2f3df909693452a3d19a21bc61d642e37fde
koki-develop <kou.pg.0131@gmail.com>
2022-05-13 18:24:14 +0900

    Footer コンポーネントに tailwindcss を適用

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,22 +1,13 @@
-import Box from '@mui/material/Box';
 import React from 'react';
 import FooterLinkList from '@/components/Layout/FooterLinkList';
 
 const Footer: React.VFC = React.memo(() => {
   return (
-    <Box
-      component='footer'
-      sx={{
-        alignItems: 'center',
-        display: 'flex',
-        flexDirection: 'column',
-        p: 2,
-      }}
-    >
+    <footer className='flex flex-col items-center p-4'>
       <small>&copy;2021</small>
 
       <FooterLinkList />
-    </Box>
+    </footer>
   );
 });
 
