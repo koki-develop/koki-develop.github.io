08955986c39b6beeb20049e02dc652892d246025
koki-develop <kou.pg.0131@gmail.com>
2022-05-13 21:32:16 +0900

    Section を拡張

--- src/components/utils/Section.tsx
+++ src/components/utils/Section.tsx
@@ -1,16 +1,16 @@
+import classNames from 'classnames';
 import React from 'react';
 import Container from '@/components/utils/Container';
 
-export type SectionProps = {
-  children: React.ReactNode;
+export type SectionProps = React.HTMLProps<HTMLDivElement> & {
   title: string;
 };
 
 const Section: React.VFC<SectionProps> = React.memo(props => {
-  const { children, title } = props;
+  const { children, className, title, ...divProps } = props;
 
   return (
-    <section className='mb-2'>
+    <section {...divProps} className={classNames(className, 'mb-2')}>
       <div className='mb-2 border-t' />
       <Container>
         <h2 className='mb-2 text-center text-3xl font-bold'>{title}</h2>
