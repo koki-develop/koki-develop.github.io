7d1689629ac5340f2e8e0dd1c1076fdc4f2f0720
koki-develop <kou.pg.0131@gmail.com>
2022-05-13 18:30:53 +0900

    Layout に tailwindcss を適用

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,10 +1,6 @@
-import Box from '@mui/material/Box';
-import CssBaseline from '@mui/material/CssBaseline';
-import { ThemeProvider } from '@mui/material/styles';
 import React from 'react';
 import Footer from '@/components/Layout/Footer';
 import Header from '@/components/Layout/Header';
-import { theme } from '@/components/Layout/theme';
 import BackToTop from '@/components/utils/BackToTop';
 
 export type LayoutProps = {
@@ -12,32 +8,21 @@ export type LayoutProps = {
 };
 
 const Layout: React.VFC<LayoutProps> = React.memo(props => {
-  return (
-    <ThemeProvider theme={theme}>
-      <LayoutContent {...props} />
-    </ThemeProvider>
-  );
-});
-
-Layout.displayName = 'Layout';
-
-const LayoutContent: React.VFC<LayoutProps> = React.memo(props => {
   const { children } = props;
 
   return (
-    <Box>
-      <CssBaseline />
+    <div>
       <BackToTop />
 
       <Header />
 
-      <Box component='main'>{children}</Box>
+      <main>{children}</main>
 
       <Footer />
-    </Box>
+    </div>
   );
 });
 
-LayoutContent.displayName = 'LayoutContent';
+Layout.displayName = 'Layout';
 
 export default Layout;
--- src/components/Layout/theme.ts
+++ src/components/Layout/theme.ts
@@ -1,3 +1,5 @@
+// TODO: ファイルごと消す
+
 import {
   createTheme,
   responsiveFontSizes,
