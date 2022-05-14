bdd5edb692355d458eb9c98ec3850a632b05bc78
koki-develop <kou.pg.0131@gmail.com>
2022-05-14 14:59:05 +0900

    title, lang を設定

--- koki-develop/app/root.tsx
+++ koki-develop/app/root.tsx
@@ -16,13 +16,13 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
-  title: 'New Remix App',
+  title: 'Koki Sato',
   viewport: 'width=device-width,initial-scale=1',
 });
 
 export default function App() {
   return (
-    <html lang='en'>
+    <html lang='ja'>
       <head>
         <Meta />
         <Links />
