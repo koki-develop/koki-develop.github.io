387cc13d5eed827719f6bd454f8906a266d357d3
koki-develop <kou.pg.0131@gmail.com>
2022-05-14 15:41:41 +0900

    import/order の設定を追加

--- koki-develop/.eslintrc.js
+++ koki-develop/.eslintrc.js
@@ -1,4 +1,48 @@
 module.exports = {
   root: true,
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling'],
+          'object',
+          'type',
+          'index',
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+        alphabetize: { order: 'asc' },
+        pathGroups: [
+          {
+            pattern: '@/app/components/Layout/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/app/components/pages/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/app/components/model/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/app/components/utils/**',
+            group: 'internal',
+            position: 'before',
+          },
+          { pattern: '@/types/**', group: 'internal', position: 'before' },
+          { pattern: '@/routes', group: 'internal', position: 'before' },
+          { pattern: '@/config', group: 'internal', position: 'before' },
+          { pattern: '@/styles/**', group: 'internal', position: 'before' },
+        ],
+      },
+    ],
+  },
   extends: ['@remix-run/eslint-config', '@remix-run/eslint-config/node'],
 };
