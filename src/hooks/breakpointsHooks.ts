78eaa6a6ad8b3c233f0e76b49a83ada45596df4e
koki-develop <kou.pg.0131@gmail.com>
2022-05-14 09:51:40 +0900

    useDown を削除

--- src/hooks/breakpointsHooks.ts
+++
@@ -1,7 +0,0 @@
-import { useMediaQuery, Breakpoint } from '@mui/material';
-import { useTheme } from '@mui/material/styles';
-
-export const useDown = (breakpoint: Breakpoint): boolean => {
-  const theme = useTheme();
-  return useMediaQuery(theme.breakpoints.down(breakpoint));
-};
