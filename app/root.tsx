417ace677f32a60231b0d6c05d5a9ada4fdd6981
koki-develop <kou.pg.0131@gmail.com>
2022-05-14 15:58:32 +0900

    meta を設定

--- app/root.tsx
+++ app/root.tsx
@@ -1,16 +1,38 @@
+import path from 'path';
 import { Links, LiveReload, Meta, Outlet, Scripts } from '@remix-run/react';
+import { config } from '@/config';
 import styles from '@/styles/app.compiled.css';
 import Layout from '@/components/Layout';
 import type { LinksFunction, MetaFunction } from '@remix-run/node';
+
+const urlJoin = (urlString: string, ...paths: string[]): string => {
+  const url = new URL(urlString);
+  url.pathname = path.join(url.pathname, ...paths);
+  return url.href;
+};
 
 export const links: LinksFunction = () => {
   return [{ rel: 'stylesheet', href: styles }];
 };
 
-export const meta: MetaFunction = () => ({
+export const meta: MetaFunction = ({ location }) => ({
   charset: 'utf-8',
-  title: 'Koki Sato',
   viewport: 'width=device-width,initial-scale=1',
+
+  title: 'Koki Sato',
+  'og:title': 'Koki Sato',
+
+  description: config.profile.description,
+  'og:description': config.profile.description,
+
+  'og:site_name': config.profile.name,
+  'og:url': urlJoin(config.url, location.pathname),
+  'og:type': 'website',
+  'og:locale': 'ja_JP',
+  'og:image': urlJoin(config.url, 'images/profile.jpg'),
+
+  'twitter:card': 'summary',
+  'twitter:site': '@koki_develop',
 });
 
 export default function App() {
