737e015aed8dae028bf0eda216f55f309ec8f921
koki-develop <kou.pg.0131@gmail.com>
2022-05-14 14:54:28 +0900

    privacy-policy ページを作成

--- koki-develop/app/components/pages/PrivacyPolicy/PrivacyPolicyPage.tsx
+++ koki-develop/app/components/pages/PrivacyPolicy/PrivacyPolicyPage.tsx
@@ -1,14 +1,10 @@
 import React, { memo } from 'react';
 import PrivacyPolicyItem from '@/components/pages/PrivacyPolicy/PrivacyPolicyItem';
 import Link from '@/components/utils/Link';
-import Meta from '@/components/utils/Meta';
-import Layout from '@/components/Layout';
 
 const PrivacyPolicyPage: React.VFC = memo(() => {
   return (
-    <Layout>
-      <Meta title='プライバシーポリシー' />
-
+    <div>
       <PrivacyPolicyItem title='免責事項'>
         当サイトのコンテンツ・情報につきまして、可能な限り正確な情報を掲載するよう努めておりますが、誤情報が入り込んだり、情報が古くなっていることもございます。当サイトに掲載された内容によって生じた損害等の一切の責任を負いかねますのでご了承ください。
       </PrivacyPolicyItem>
@@ -33,7 +29,7 @@ const PrivacyPolicyPage: React.VFC = memo(() => {
       <PrivacyPolicyItem title='プライバシーポリシーの変更について'>
         当サイトは、個人情報に関して適用される日本の法令を遵守するとともに、本ポリシーの内容を適宜見直しその改善に努めます。修正された最新のプライバシーポリシーは常に本ページにて開示されます。
       </PrivacyPolicyItem>
-    </Layout>
+    </div>
   );
 });
 
