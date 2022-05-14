568e3c575ab9714a264044aa52141d07c3dae5e4
koki-develop <kou.pg.0131@gmail.com>
2022-05-14 14:43:01 +0900

    config.ts, routes.ts をコピー

---
+++ koki-develop/app/routes.ts
@@ -0,0 +1,17 @@
+export class Routes {
+  public static home(): string {
+    return '/';
+  }
+
+  public static privacyPolicy(): string {
+    return '/privacy-policy';
+  }
+
+  public static repositories(): string {
+    return 'https://github.com/koki-develop?tab=repositories&type=source';
+  }
+
+  public static mailto(email: string): string {
+    return `mailto:${email}`;
+  }
+}
