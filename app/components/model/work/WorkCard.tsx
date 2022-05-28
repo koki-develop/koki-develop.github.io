936321d00804de04f3b4b0fef0231ea79512fda6
koki-develop <kou.pg.0131@gmail.com>
2022-05-28 22:09:02 +0900

    WorkCard のトップに div を追加

--- app/components/model/work/WorkCard.tsx
+++ app/components/model/work/WorkCard.tsx
@@ -13,55 +13,57 @@ const WorkCard: React.VFC<WorkCardProps> = memo(props => {
   const { work } = props;
 
   return (
-    <Card>
-      {work.hasImage && (
-        <Link className='relative overflow-hidden' external href={work.url}>
-          <div className='absolute h-36 w-full rounded-t bg-gray-500 opacity-0 transition hover:opacity-10 lg:h-48' />
-          <div className='h-36 w-full lg:h-48'>
-            <img
-              className='h-full w-full rounded-t object-cover'
-              style={{
-                objectPosition: work.imagePosition ?? 'center',
-              }}
-              src={path.join('/images/works', `${work.name}.png`)}
-              alt={work.name}
-            />
-          </div>
-        </Link>
-      )}
-      <div className='p-2 px-4 pb-4 text-sm'>
-        <h3 className='mb-1 text-2xl font-bold'>
-          <Link external href={work.url}>
-            {work.name}
+    <div>
+      <Card>
+        {work.hasImage && (
+          <Link className='relative overflow-hidden' external href={work.url}>
+            <div className='absolute h-36 w-full rounded-t bg-gray-500 opacity-0 transition hover:opacity-10 lg:h-48' />
+            <div className='h-36 w-full lg:h-48'>
+              <img
+                className='h-full w-full rounded-t object-cover'
+                style={{
+                  objectPosition: work.imagePosition ?? 'center',
+                }}
+                src={path.join('/images/works', `${work.name}.png`)}
+                alt={work.name}
+              />
+            </div>
           </Link>
-        </h3>
-        <div className='mb-2 flex flex-row-reverse items-center justify-end pl-1'>
-          {work.skills
-            .concat()
-            .reverse()
-            .map((skill, i) => (
-              <Tooltip
-                key={skill.name}
-                className='-ml-2 hover:z-50'
-                text={skill.name}
-              >
-                <div className='h-6 w-6 overflow-hidden rounded-full border border-gray-400 bg-white transition'>
-                  <img
-                    className='h-full w-full'
-                    src={path.join('/images/icons', `${skill.name}.svg`)}
-                    alt={skill.name}
-                  />
-                </div>
-              </Tooltip>
-            ))}
-        </div>
-        <p className='mb-2'>{work.description}</p>
+        )}
+        <div className='p-2 px-4 pb-4 text-sm'>
+          <h3 className='mb-1 text-2xl font-bold'>
+            <Link external href={work.url}>
+              {work.name}
+            </Link>
+          </h3>
+          <div className='mb-2 flex flex-row-reverse items-center justify-end pl-1'>
+            {work.skills
+              .concat()
+              .reverse()
+              .map((skill, i) => (
+                <Tooltip
+                  key={skill.name}
+                  className='-ml-2 hover:z-50'
+                  text={skill.name}
+                >
+                  <div className='h-6 w-6 overflow-hidden rounded-full border border-gray-400 bg-white transition'>
+                    <img
+                      className='h-full w-full'
+                      src={path.join('/images/icons', `${skill.name}.svg`)}
+                      alt={skill.name}
+                    />
+                  </div>
+                </Tooltip>
+              ))}
+          </div>
+          <p className='mb-2'>{work.description}</p>
 
-        <Link external href={work.repositoryUrl}>
-          View on GitHub
-        </Link>
-      </div>
-    </Card>
+          <Link external href={work.repositoryUrl}>
+            View on GitHub
+          </Link>
+        </div>
+      </Card>
+    </div>
   );
 });
 
