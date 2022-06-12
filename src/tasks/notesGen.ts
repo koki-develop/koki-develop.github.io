86f05e09f60eaa9c0609a0a4213667bcca5aac3a
koki-develop <kou.pg.0131@gmail.com>
2022-06-12 19:11:10 +0900

    notes:gen スクリプトを作成

---
+++ src/tasks/notesGen.ts
@@ -0,0 +1,12 @@
+import * as fs from 'fs';
+import * as path from 'path';
+import * as Parser from 'rss-parser';
+
+(async () => {
+  const parser = new Parser();
+  const { items } = await parser.parseURL('https://zenn.dev/kou_pg_0131/feed');
+  fs.writeFileSync(
+    path.join(process.cwd(), 'src/notes.json'),
+    JSON.stringify(items),
+  );
+})();
