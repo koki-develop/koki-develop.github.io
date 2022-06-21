7f85e15e88051ad85a3858b20bdaed3df18c82d3
koki-develop <kou.pg.0131@gmail.com>
2022-06-22 08:30:05 +0900

    一部修正

--- src/components/model/skill/SkillCardList.tsx
+++ src/components/model/skill/SkillCardList.tsx
@@ -13,7 +13,7 @@ const SkillCardList: React.VFC<SkillCardListProps> = memo(props => {
     <div className='mb-4'>
       {skillGroups.map(group => (
         <div className='mb-4' key={group.name}>
-          <h3 className='mb-2 text-center font-bold'>{group.name}</h3>
+          <h3 className='mb-2 text-center'>{group.name}</h3>
           <div className='grid grid-cols-2 gap-2 sm:grid-cols-3 sm:gap-3 md:grid-cols-4 md:gap-4'>
             {group.skills.map(skill => (
               <SkillCard key={skill.name} skill={skill} />
