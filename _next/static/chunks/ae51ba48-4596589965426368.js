cee02985bd3c392b818d48d495392ed8076b0a18
koki-develop <koki-develop@users.noreply.github.com>
2024-03-28 15:06:15 +0000

    deploy: 1b7298f7738b5cf1c877f40700a1818e6908c0e3

---
+++ _next/static/chunks/ae51ba48-4596589965426368.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[260],{155:function(t,n,e){e.d(n,{mzm:function(){return i}});var r=e(8357);function i(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{fill:"none",strokeLinecap:"round",strokeLinejoin:"round",strokeWidth:"48",d:"M184 112l144 144-144 144"}}]})(t)}}}]);
